0d0d726110a508a3ae5d1b5a9d67010f35fb49b4
brokestar7 <82914463+brokestar7@users.noreply.github.com>
2021-06-29 18:52:37 +0200

    programando la funcion generar vidas
    he puesto el resto del java script en comentario para que no me moleste a la hora de ver el resultado. luego ire quitando comentarios a medida que vayamos programando las funciones

--- Proyecto Juego IP/Proyecto Juego.js
+++ Proyecto Juego IP/Proyecto Juego.js
@@ -1,25 +1,32 @@
-function  iniciarJuego() {
-    generarVidas();
-    randomizar();
-}
-function randomizar() {
-    generarFondo();
-    generarEnemigo();
-}
-generarCombate();
-function generarCombate(){
-    mostrarLayoutAtaques();
 
+// function  iniciarJuego() {
+//     generarVidas();
+//     randomizar();
+// }
+function generarVidas() {
+console.log("entra en la funcion");
+    for(i=0;i<=20;i++) {
+    document.getElementById("contenedorCorazones").innerHTML += "<img src='Imagenes proyecto juego/imagenCorazon.png'>"; 
 }
-function generarLayoutAtaques(){
-    compararCombate();
 }
-function compararCombate(ataqueEscogido) {
-    //aqui se comparara el los ataques como si fuera un piedra papel y tijeras
-    quitarVidaHumano();
-    quitarVidaEnemigo();
-    restarMp();
-}
-function generarEnemigo(){
-    generarAtaquesEnemigo();
-}
+// function randomizar() {
+//     generarFondo();
+//     generarEnemigo();
+// }
+// generarCombate();
+// function generarCombate(){
+//     mostrarLayoutAtaques();
+
+// }
+// function generarLayoutAtaques(){
+//     compararCombate();
+// }
+// function compararCombate(ataqueEscogido) {
+//     //aqui se comparara el los ataques como si fuera un piedra papel y tijeras
+//     quitarVidaHumano();
+//     quitarVidaEnemigo();
+//     restarMp();
+// }
+// function generarEnemigo(){
+//     generarAtaquesEnemigo();
+// }
