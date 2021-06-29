2f8dc1b5aeeb64c54fdce70237626f68db06ab0c
brokestar7 <82914463+brokestar7@users.noreply.github.com>
2021-06-29 19:51:55 +0200

    trabajando en la funcion generarFondo

--- Proyecto Juego IP/Proyecto Juego.js
+++ Proyecto Juego IP/Proyecto Juego.js
@@ -1,18 +1,23 @@
 
-// function  iniciarJuego() {
-//     generarVidas();
-//     randomizar();
-// }
+function  iniciarJuego() {
+     generarVidas();
+     randomizar();
+}
 function generarVidas() {
 console.log("entra en la funcion");
     for(i=0;i<=20;i++) {
-    document.getElementById("contenedorCorazones").innerHTML += "<img src='Imagenes proyecto juego/imagenCorazon.png'>"; 
+    document.getElementById("contenedorCorazones").innerHTML += "<img id='imgCorazon' src='Imagenes proyecto juego/imagenCorazon.png'>"; 
 }
 }
-// function randomizar() {
-//     generarFondo();
-//     generarEnemigo();
-// }
+function randomizar() {
+    generarFondo();
+   // generarEnemigo();
+} 
+function generarFondo() {
+    numeroAleatorioFondo= Math.floor(Math.random () *10);
+    document.getElementById("fondoJuego").innerHTML = "<img src='Imagenes proyecto juego/fondoMontaña1.gif'>"
+
+}
 // generarCombate();
 // function generarCombate(){
 //     mostrarLayoutAtaques();
