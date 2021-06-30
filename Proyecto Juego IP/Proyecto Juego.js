5922e05d3cbaa02232e7be6ee78f45ea6d271364
danielDunno <danielherzm@gmail.com>
2021-06-30 18:26:00 +0200

    Funcion generar Enemigo aleatorio hecha
    generar numero aleatorio que selecciona el gif(de momento solo son 3 de prueba ya incorporaremos los definitvos) estaria bien que tuviesen dimensiones parecidas

--- Proyecto Juego IP/Proyecto Juego.js
+++ Proyecto Juego IP/Proyecto Juego.js
@@ -7,17 +7,26 @@ function generarVidas() {
 console.log("entra en la funcion");
     for(i=0;i<=20;i++) {
     document.getElementById("contenedorCorazones").innerHTML += "<img id='imgCorazon' src='Imagenes proyecto juego/imagenCorazon.png'>"; 
-}
+    }
 }
 function randomizar() {
     generarFondo();
-   // generarEnemigo();
+    generarEnemigo();
 } 
 function generarFondo() {
     numeroAleatorioFondo= Math.floor(Math.random () *10);
-    document.getElementById("fondoJuego").innerHTML = "<img src='Imagenes proyecto juego/fondoMontaña1.gif'>"
+    document.getElementById("fondoJuego").innerHTML = "<img id='fondo'src='Imagenes proyecto juego/fondoPraderaDia.jpg'>";
 
 }
+function generarEnemigo() {
+    numeroAleatorioEnemigo = Math.floor(Math.random () *3)+1;
+    alert(numeroAleatorioEnemigo);
+    document.getElementById("enemigo").innerHTML = "<img class='spriteEnemigo' src='Imagenes enemigos/" + numeroAleatorioEnemigo + ".gif'>";
+    //class spriteEnemigo para despues modificarlo en el css
+}
+
+
+
 // generarCombate();
 // function generarCombate(){
 //     mostrarLayoutAtaques();
