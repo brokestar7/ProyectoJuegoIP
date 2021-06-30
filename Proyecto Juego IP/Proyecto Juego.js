ba5908f9d0275757650ef8f5ff951a02d4d28ac8
danielDunno <danielherzm@gmail.com>
2021-06-30 19:51:29 +0200

    personajes + corazon negro del enemigo

--- Proyecto Juego IP/Proyecto Juego.js
+++ Proyecto Juego IP/Proyecto Juego.js
@@ -1,4 +1,8 @@
 
+let arrFondos=["fondoCastillo1.gif","fondoMontaña1.gif","fondoRioDia.gif","fondoRioDia2.gif"];
+let numeroAleatorioEnemigo;
+let numeroAleatorioFondo;
+
 function  iniciarJuego() {
      generarVidas();
      randomizar();
@@ -13,23 +17,34 @@ function randomizar() {
     generarFondo();
     generarEnemigo();
 } 
+
 function generarFondo() {
-    numeroAleatorioFondo= Math.floor(Math.random () *10);
-    document.getElementById("fondoJuego").innerHTML = "<img id='fondo'src='Imagenes proyecto juego/fondoPraderaDia.jpg'>";
+numeroAleatorioFondo= Math.floor(Math.random () *4);
+document.getElementById("fondoJuego").innerHTML = "<img id='imagenFondo' src='Imagenes proyecto juego/"+arrFondos[numeroAleatorioFondo]+"'>"
 
 }
+
 function generarEnemigo() {
     numeroAleatorioEnemigo = Math.floor(Math.random () *3)+1;
     alert(numeroAleatorioEnemigo);
     document.getElementById("enemigo").innerHTML = "<img class='spriteEnemigo' src='Imagenes enemigos/" + numeroAleatorioEnemigo + ".gif'>";
     //class spriteEnemigo para despues modificarlo en el css
+    //generarVidasEnemigo();
+    //generarCombate();
 }
 
+function generarVidasEnemigo(){
 
+    for(i=0;i<=20;i++) {
+        document.getElementById("contenedorCorazones").innerHTML += "<img id='imgCorazon' src='Imagenes proyecto juego/imagenCorazon.png'>"; 
+        }
 
-// generarCombate();
+}
+
 // function generarCombate(){
 //     mostrarLayoutAtaques();
+
+
 
 // }
 // function generarLayoutAtaques(){
