9aa7c67d22e22f88fc2fa75e1a0070e5202f3ff2
AniuLu <85352949+AniuLu@users.noreply.github.com>
2021-07-07 18:54:29 +0200

    link ataques

--- Proyecto Juego IP/Proyecto Juego.js
+++ Proyecto Juego IP/Proyecto Juego.js
@@ -1,5 +1,5 @@
 let arrFondos=["fondoCastillo1.gif","fondoMontaña1.gif","fondoRioDia.gif","fondoRioDia2.gif"];
-let arrAtaques=["Ataque","Ataque","Ataque","Vida","Defensa"];
+let arrAtaques=["Ataque 1","Ataque 2","Ataque 3","Vida","Defensa"];
 let arrImgAtaques=["espada4.png","espada11.gif","espada12.gif","pocionVida.png","shield.png"];
 let numeroAleatorioEnemigo;
 let numeroAleatorioFondo;
@@ -43,6 +43,7 @@ function generarEnemigo() {
     document.getElementById("enemigo").innerHTML = "<img class='sprites' src='Imagenes enemigos/" + numeroAleatorioEnemigo + ".gif'>";
     generarVidasEnemigo();
     generarCombate();
+    generarAtaquesEnemigo();
 }
 
 function generarVidasEnemigo() {
@@ -59,22 +60,41 @@ function inicializar() {
 //5 botones de ataque del usuario (3 ataques, 1 defensa y 1 pocima de recuperación)
 function generarCombate(){
   /* for (i=0;i<5;i++){
-        document.getElementById("botonesAtaques").innerHTML+=
-        "<a href='#' id='botonesAtaques"+i+"' onclick='generarLayoutTotales("+i+")'> "+"<img src='Imagenes objetos/"+arrImgAtaques[i]+"' width='25px' height='25px' >"+" </a> ";
+        document.getElementById("botonesAtaques").innerHTML
+        "<a href='#' id='botonesAtaques"+i+"' onclick='generarLayoutAtaques("+i+")'> "+"<img src='Imagenes objetos/"+arrImgAtaques[i]+"' width='50px' height='50px' >"+" </a> ";
         compararCombate();
     }*/
 
-    for (i=0;i<5;i++){
-        document.getElementById("botonesAtaques").innerHTML=
-        "<a href='#' id='botonesAtaques0' onclick='generarLayoutTotales(0)'> "+"<img src='Imagenes objetos/"+arrImgAtaques[0]+"' width='50px' height='50px' >"+" </a> <br>"+
-        "<a href='#' id='botonesAtaques1' onclick='generarLayoutTotales(1)'> "+"<img src='Imagenes objetos/"+arrImgAtaques[1]+"' width='50px' height='50px' >"+" </a> <br>"+
-        "<a href='#' id='botonesAtaques2' onclick='generarLayoutTotales(2)'> "+"<img src='Imagenes objetos/"+arrImgAtaques[2]+"' width='50px' height='50px' >"+" </a> <br>"+
-        "<a href='#' id='botonesAtaques3' onclick='generarLayoutTotales(3)'> "+"<img src='Imagenes objetos/"+arrImgAtaques[3]+"' width='50px' height='50px' >"+" </a> <br>"+
-        "<a href='#' id='botonesAtaques4' onclick='generarLayoutTotales(4)'> "+"<img src='Imagenes objetos/"+arrImgAtaques[4]+"' width='50px' height='50px' >"+" </a> ";
-        compararCombate();
-    }
+ 
+    document.getElementById("botonesAtaques").innerHTML=
+    "<br><a href='#' class='botonesAtaques0' onclick='generarLayoutTotales(0)'> "+arrAtaques[0]+" <img src='Imagenes objetos/"+arrImgAtaques[0]+"' width='50px' height='50px' >"+" </a> <br>"+
+    "<a href='#' class='botonesAtaques1' onclick='generarLayoutTotales(1)'> "+arrAtaques[1]+" <img src='Imagenes objetos/"+arrImgAtaques[1]+"' width='50px' height='50px' >"+" </a> <br>"+
+    "<a href='#' class='botonesAtaques2' onclick='generarLayoutTotales(2)'> "+arrAtaques[2]+" <img src='Imagenes objetos/"+arrImgAtaques[2]+"' width='50px' height='50px' >"+" </a> <br>"+
+    "<a href='#' class='botonesAtaques3' onclick='generarLayoutTotales(3)'> "+arrAtaques[3]+" <img src='Imagenes objetos/"+arrImgAtaques[3]+"' width='50px' height='50px' >"+" </a> <br>"+
+    "<a href='#' class='botonesAtaques4' onclick='generarLayoutTotales(4)'> "+arrAtaques[4]+" <img src='Imagenes objetos/"+arrImgAtaques[4]+"' width='50px' height='50px' >"+" </a> <br>";
+    compararCombate();
+    
 }
+
+function generarAtaquesEnemigo(){
+    numeroAletorioAtaque=Math.floor(Math.random() * 3);
+}
+
+ function generarAtaques(ataqueEscogido) {
+//     //aqui se comparara el los ataques como si fuera un piedra papel y tijeras
+    console.log("Usuario "+ataqueEscogido);
+    //si el ataque escogido es 0
+      
+
+    }
+//     quitarVidaHumano();
+//     quitarVidaEnemigo();
+//     restarMp();
 // }
+
+ 
+
+ // }
 // function generarLayoutAtaques(){
 //     compararCombate();
 // }
